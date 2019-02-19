79d0f52c1e4753cbba811cd3626d07795ac99269
Ted <ted@renocompass.ca>
2019-02-19 21:03:39 +0800

    assign capability function

--- wordpress/wp-content/plugins/ABAC/assign_permission.php
+++ wordpress/wp-content/plugins/ABAC/assign_permission.php
@@ -1,13 +1,14 @@
 <?php
 global $current_user, $selected_user, $task_description, $current_selected_permissions, $time;
 
+function connect_sql(){
+    $dbLocalhost = mysqli_connect("localhost", "root", "root", "wordpress")
+        or die("Could not connect: " . mysql_error());
+    return $dbLocalhost;
+}
 
-function assign_task(){
-    ?>
-    <h1>Assign task page</h1>
-    <?php
-         //task information as global variables
-        // Grab current user information.
+function print_current_user_information(){
+        // Grab current user information and print out all the information.
         $current_user = wp_get_current_user();
         $currentUser = $current_user->user_login;
         echo "<h3>You information</h3>";
@@ -17,18 +18,25 @@ function assign_task(){
         echo 'User last name: ' . $current_user->user_lastname . '<br />';
         echo 'User display name: ' . $current_user->display_name . '<br />';
         echo 'User ID: ' . $current_user->ID . '<br />';
+        return $currentUser;
+}
 
-        
+
+function assign_task(){
+    ?>
+    <h1>Assign task page</h1>
+    <?php
+        //print out current user information
+        $currentUser = print_current_user_information();
         //database connection
-        $dbLocalhost = mysqli_connect("localhost", "root", "root", "wordpress")
-        or die("Could not connect: " . mysql_error());
+        $dbLocalhost = connect_sql();
 
         // check who gets supervise by the current user.
-            echo "<h3>You are supervising the following users</h3>";
+            
             $supervisorOf = "SELECT `user_login` FROM `wp_users` WHERE `supervisor`='$currentUser'";
-            if ($info = mysqli_query($dbLocalhost, $supervisorOf))
-            {      
-                echo 'Data retrived successfully';
+            if ($info = mysqli_query($dbLocalhost, $supervisorOf)){      
+                echo '<h4>Data retrived successfully</h4>';
+                echo "<h3>You are supervising the following users</h3>";
                 $userInfo = mysqli_fetch_assoc($info);
                 foreach($userInfo as $user_info){
                     echo '<ul class="list-group">';
@@ -38,16 +46,14 @@ function assign_task(){
                     echo '<ul>';
                 }
             }
-            else
-            {
+            else{
                 echo mysqli_error($dbLocalhost);
             }  
         
-            echo '<form action="" method="post">';
+            echo '<form name = "first" action="" method="post">';
             echo '<div class="form-group">';
             echo '<label for="user">Please select the user you want to assign task to </label>';
             echo "<select name='user' class='form-control'>";
-
             foreach($userInfo as $user_info) {
                 echo "<option value='".$user_info."'>".$user_info."</option>";
             }
@@ -60,19 +66,27 @@ function assign_task(){
             //if exist valid input, then assign value to $user and show the list of the permissions
             if(isset($_POST['user'])) {
                 $selected_user =$_POST["user"];
+                // echo $selected_user;
                 $user_role = get_user_by('login', $selected_user)->roles[0]; //get curreny user role
                 $capability_set = array_keys(get_role($user_role)->capabilities); //get selected user capability set
                 $current_capability = array_keys(get_role(restrictly_get_current_user_role())->capabilities);
                 $diff = array_values(array_diff($current_capability,$capability_set)); //permission difference between selected user and current user
                 
-                echo '<form action="" method="post">';
+                echo '<form name="second" action="" method="post">';
                 echo '<div class="form-group">';
-                echo '<label for="current_selected_permissions">Please select the permission you want to assign the person. </label>';
+                echo '<label for="current_selected_permissions">Please select the permission you want to assign the person. </label><br>';
+                
                 echo "<select name='current_selected_permissions' class='form-control'>";
+                echo "<option value='".$selected_user."'>".$selected_user."</option>";
+                echo "</select><br>";
+                
+                echo "<select name='current_user_name' class='form-control'>";
                 foreach($diff as $diff_permissions){
                     echo "<option value='".$diff_permissions."'>".$diff_permissions."</option>";
                 }
                 echo '</select><br>';
+
+
                 echo '<label class="sr-only" for="task_description">Please enter the task description.</label>';
                 echo '<input type="text" class="form-control" name="task_description" placeholder="Please enter the task description."><br>';
                 echo '<input class="btn btn-primary btn-lg btn-block" type="submit" value="Submit">';
@@ -81,13 +95,19 @@ function assign_task(){
             }
 
             //assign selected permission and task information
-            if(isset($_POST['current_selected_permissions'],$_POST['task_description'])){
-                global $current_selected_permissions, $task_description;
+            if(isset($_POST['current_selected_permissions'],$_POST['task_description'],$_POST['current_user_name'])){
                 $current_selected_permissions=$_POST['current_selected_permissions'];
                 $task_description=$_POST['task_description'];
+                $current_user_name = $_POST['current_user_name'];
             }
+
+            if (!empty($current_selected_permissions) && !empty($task_description) && !empty($currentUser)){
+                echo $current_selected_permissions;
+                echo $task_description;
+                echo $currentUser;
+                echo date('Y-m-d h:i:s', time());
+            }
+            
 }
 
-//get current role and capability and save it, create a new role with new set of permissions and assign to this user for 2 h. after 2h, 
-//role deleted and origional role assign back. 
 ?>
